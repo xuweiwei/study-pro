1d9fc4012c2c087d58fc572abe8e8d31132f81f5
wangfl <wangfl@parox.cn>
2015-04-15 09:20:24 +0800

    Initial Pro

---
+++ src/main/java/com/philip/junit/HelloWorldTest.java
@@ -0,0 +1,16 @@
+package com.philip.junit;
+
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+//import com.philip.test.HelloWorld;
+
+
+public class HelloWorldTest {
+
+//	@Test
+	public void testSayHello(){
+//		HelloWorld hw = new HelloWorld();
+//		Assert.assertEquals("Hello philip", hw.sayHello("philip"));
+	}
+}
---
+++ src/main/java/com/philip/test/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.philip.test;
+
+public class HelloWorld {
+
+	public String sayHello(String name){
+		return "Hello " + name ;
+	}
+}
